b1dd137c5185161ac19fbb3c60ad41960f411c18
verdgil <me@verdgil.org>
2020-12-09 23:42:19 +0300

    КОООСТЫЛИ)

--- os_api/views.py
+++ os_api/views.py
@@ -48,9 +48,9 @@ class OSSSHView(WebsocketConsumer):
                             self.qemu_proc = subprocess.Popen(start_string[1].split(' '))
                             self.send("Запущено, ожидаем включения")
                             # sleep(1*30)
-                            self.send(("НЕ РАБОТАЕТ ФРОНТОВАЯ ЧАСТЬ, СОКЕТ РВЁТСЯ, " +
-                                      "ВЫ МОЖЕТЕ ПОДКЛЮЧИТСЯ ПО SSH К ТЕСТОВОВ СИСТЕМЕ КОМАНДОЙ ssh root@unix-history.org:%s " +
-                                      "Пароль - uh") % (self.port_num))
+                            # self.send(("НЕ РАБОТАЕТ ФРОНТОВАЯ ЧАСТЬ, СОКЕТ РВЁТСЯ, " +
+                            #           "ВЫ МОЖЕТЕ ПОДКЛЮЧИТСЯ ПО SSH К ТЕСТОВОВ СИСТЕМЕ КОМАНДОЙ ssh root@unix-history.org:%s " +
+                            #           "Пароль - uh") % (self.port_num))
                             # stdin, stdout, stderr = client.exec_command('ls -l')
                             # data = stdout.read() + stderr.read()
                     self.send(start_string[1])
@@ -60,13 +60,18 @@ class OSSSHView(WebsocketConsumer):
             self.close()
 
     def receive(self, text_data=None, bytes_data=None):
-        self.client = paramiko.SSHClient()
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect(hostname="127.0.0.1", username="root", password="uh", port=self.port_num)
         if text_data is not None:
             print(text_data)
-            stdin, stdout, stderr = self.client.exec_command(text_data)
-            self.send(stdout.read() + stderr.read())
+        try:
+            self.client = paramiko.SSHClient()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.client.connect(hostname="127.0.0.1", username="root", password="uh", port=self.port_num)
+            if text_data is not None:
+                print(text_data)
+                stdin, stdout, stderr = self.client.exec_command(text_data)
+                self.send(stdout.read() + stderr.read())
+        except:
+            pass
 
     def disconnect(self, message):
         if self.qemu_proc is not None:
