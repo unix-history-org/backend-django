a9952875a6bd6862bdbee98b0f459a8bf8cbf4d4
verdgil <me@verdgil.org>
2021-04-10 22:54:03 +0300

    fix cors (?)

--- unixhistory/base.py
+++ unixhistory/base.py
@@ -50,17 +50,17 @@ INSTALLED_APPS = [
     'os_api',
     'user',
 ]
-
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'unixhistory.urls'
 
