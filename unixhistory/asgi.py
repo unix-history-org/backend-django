619256b9f8a55fd74092939cca0d689d2c394470
verdgil <me@verdgil.org>
2021-05-20 21:58:16 +0300

    fix asgi

--- unixhistory/asgi.py
+++ unixhistory/asgi.py
@@ -9,21 +9,27 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
 # models.
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unixhistory.settings")
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from os_api import urls as os_api_urls
+from os_api.views import OSSSHView
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
 
     # WebSocket chat handler
-    "websocket": os_api_urls.websocket_urlpatterns,
+    "websocket": URLRouter(
+        url(r'api/os/<int:pk>/ssh', OSSSHView.as_asgi()),
+    ),
 })
